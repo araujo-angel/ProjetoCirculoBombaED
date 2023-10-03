1313cbd6e437f9aadfbae910a6980a0b931d6cc6
Ananda Guedes do Ó <anandaguedesdoo@gmail.com>
2023-10-03 16:36:47 -0300

    alterações

--- jogo.py
+++ jogo.py
@@ -12,7 +12,10 @@ class Jogo:
 
 
     def __criar__(self):
-        rodada = Lista.inserir # o que está sendo inserido? Onde está sendo inserido?
+        '''
+        Função para a construção da rodada como uma lista.
+        '''
+        rodadaParticipantes = Lista.inserir # o que está sendo inserido? Onde está sendo inserido?
         return rodada
 
 
@@ -20,65 +23,35 @@ class Jogo:
         """
         Função que têm a lógica do jogo e todo seu funcionamento.
         """
-        j = 1
-        r = 0 # rodada do jogo
+        j = 1 #o que é esse j?
+        rodada = 0 #contador de rodadas no jogo
 
         while len(rodada) > qtdVencedores:
-            r += 1
-            print(f'Participantes {rodada}')
-            print(f'Rodada: {r}')
+            rodada += 1 
+            print(f'Participantes: {rodadaParticipantes}')
+            print(f'Rodada: {rodada}')
 
-            temp = random.randint(4, 16)
-            for i in range(temp):
-                if j > len(rodada):
-                    j = 1
-                # como volto para o 0 se o temp for maior que o tamanho da lista?
-        jogo.close()
-
-
-#professor aula remota
-pilha = []
-    lst.preparaPercurso(1)
-    step = 0
-    while (step < 3): # A musica vai tocar e parar 3 vezes
-        cont = 0
-        while(lst.temProximo()):
-            carga = lst.pedirProximo()
-            print(carga)
-            cont += 1
-            if (cont == 5):
-                break
-        posicao = lst.busca(carga)
-        pilha.append(carga)
-        lst.remover(posicao)
-        print(lst)
-        step += 1
-
-
-#relação professor -> nosso código
-    Lista.preparaPercurso(1)
-    temp =  random.randint(4, 16)
-    while (temp < qtdVencedores):
-        cont = 0
-        while(Lista.temProximo()):
-            carga = Lista.pedirProximo()
-            print(carga)
-            cont += 1
-            if (cont < len(Lista)-1):
-                break
-        posicao = Lista.busca(carga)
-        Pilha.empilha(carga)
-        print(f'Removido: {carga}')
-        Lista.remover(posicao)
-        #print(f'Participantes: {Lista}')
-        temp += 1
-
+            Lista.preparaPercurso(1)
+            temp =  random.randint(4, 16)
+            while (temp > qtdVencedores):
+                cont = 0
+                while(Lista.temProximo()):
+                    carga = Lista.pedirProximo()
+                    print(carga)
+                    cont += 1
+                    if (cont < len(Lista)-1):
+                        break
+                posicao = Lista.busca(carga)
+                Pilha.empilha(carga)
+                print(f'Removido: {carga}')
+                Lista.remover(posicao)
+                #print(f'Participantes: {Lista}')
+                temp += 1
                 if temp == i+1:
                     removido = rodada.remover(j)
                     pilha.empilha(removido)
                     print(f'Removido: {removido}')
                 j += 1
-
 
     def __str__(self)->str:
         s = 'Eliminados: '
@@ -96,3 +69,26 @@ pilha = []
         """
         jogo = open('jogo.txt','a')
         jogo.write (str) # o que é isso? esta certo mesmo?
+
+
+
+#professor aula remota
+# pilha = []
+#     lst.preparaPercurso(1)
+#     step = 0
+#     while (step < 3): # A musica vai tocar e parar 3 vezes
+#         cont = 0
+#         while(lst.temProximo()):
+#             carga = lst.pedirProximo()
+#             print(carga)
+#             cont += 1
+#             if (cont == 5):
+#                 break
+#         posicao = lst.busca(carga)
+#         pilha.append(carga)
+#         lst.remover(posicao)
+#         print(lst)
+#         step += 1
+
+
+
