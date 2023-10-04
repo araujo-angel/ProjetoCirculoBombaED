3fc22e2e62ea98863ee848c63e546025c07913e2
Angêlica Araújo <134542537+araujo-angel@users.noreply.github.com>
2023-10-04 15:11:52 -0300

    reorganização total do projeto.

--- jogo.py
+++ jogo.py
@@ -1,4 +1,4 @@
-from ListaSimplesmenteEncadeada import *
+from ListaEncadeadaCircular import *
 from PilhaSimplesmenteEncadeada import *
 import random
 
@@ -7,92 +7,68 @@ class Jogo:
         """
         No construtor da classe jogo, criamos os atributos jogadores e removidos e instanciamos a lista e a pilha nos mesmos.
         """
-        self.__jogadores = Lista()
-        self.__removidos = Pilha()
+        self.__jogadores = Lista() #Aqui nossa lista é instanciada
+        self.__removidos = Pilha() #Aqui nossa pilha é instanciada
+        self.__qtdVencedores = 0
 
-
-    def __criar__(self):
-        rodada = Lista.inserir # o que está sendo inserido? Onde está sendo inserido?
-        return rodada
-
-
-    def __inicializador__(self, qtdVencedores):
+    def addParticipantes(self, posicao, participante):
         """
-        Função que têm a lógica do jogo e todo seu funcionamento.
+        Método que adiciona os participantes na lista.
         """
-        j = 1
-        r = 0 # rodada do jogo
-
-        while len(rodada) > qtdVencedores:
-            r += 1
-            print(f'Participantes {rodada}')
-            print(f'Rodada: {r}')
-
-            temp = random.randint(4, 16)
-            for i in range(temp):
-                if j > len(rodada):
-                    j = 1
-                # como volto para o 0 se o temp for maior que o tamanho da lista?
-        jogo.close()
-
-
-#professor aula remota
-pilha = []
-    lst.preparaPercurso(1)
-    step = 0
-    while (step < 3): # A musica vai tocar e parar 3 vezes
-        cont = 0
-        while(lst.temProximo()):
-            carga = lst.pedirProximo()
-            print(carga)
-            cont += 1
-            if (cont == 5):
-                break
-        posicao = lst.busca(carga)
-        pilha.append(carga)
-        lst.remover(posicao)
-        print(lst)
-        step += 1
-
-
-#relação professor -> nosso código
-    Lista.preparaPercurso(1)
-    temp =  random.randint(4, 16)
-    while (temp < qtdVencedores):
-        cont = 0
-        while(Lista.temProximo()):
-            carga = Lista.pedirProximo()
-            print(carga)
-            cont += 1
-            if (cont < len(Lista)-1):
-                break
-        posicao = Lista.busca(carga)
-        Pilha.empilha(carga)
-        print(f'Removido: {carga}')
-        Lista.remover(posicao)
-        #print(f'Participantes: {Lista}')
-        temp += 1
-
-                if temp == i+1:
-                    removido = rodada.remover(j)
-                    pilha.empilha(removido)
-                    print(f'Removido: {removido}')
-                j += 1
-
-
-    def __str__(self)->str:
-        s = 'Eliminados: '
-        cursor = self.__topo
-        while( cursor is not None ):
-            s += f'{cursor.carga}, '
-            cursor = cursor.prox
-        s = s.rstrip('< ')
-        s += ' '
-        return s
-
-    def salvar_jogo(nome):
+        self.__jogadores.inserir(posicao, participante)#aqui nossa objeto da classe lista insere os jogadores
+    
+    def addQtdVencedores(self, qtdVencedores):
         """
-        Método que salva a rodada do jogo atual.
+        Método que recebe a quantidade de vencedores.
         """
-        jogo = open('jogo.txt','a')
-        jogo.write (str) # o que é isso? esta certo mesmo?
+        self.__qtdVencedores = qtdVencedores
+
+    def rodada(self, num):
+        """
+        Método que gera as rodadas do jogo de acordo com as regras do mesmo.
+        """
+        temp = random.randint(4, 16)
+        print(f'Participantes: {self.__jogadores}') #nossa lista de jogadores
+        print(f'Rodada: {num}') #numero da rodada
+        print(f'Pointer: {self.__jogadores.pointer()}') #jogador/a da vez
+        print(f'K {temp}')#numero de voltas
+        for i in range(temp):
+            carga = self.__jogadores.pedirProximo()
+            if i+1 == temp:
+                posicao = self.__jogadores.busca(carga)
+                self.__removidos.empilha(carga)#aqui quem foi removido é passado pra pilha de removidos
+                print(f'Removido: {carga}')
+                self.__jogadores.remover(posicao)#aqui ele/ela é deletado
+
+
+    def executar(self):
+        """
+        Método que executa jogo.
+        """
+        self.__jogadores.preparaPercurso(1)#prepara o ponteiro para percorrer a lista a partir do nó correspondente à posicao indicada, no caso, 1.
+        num_rodada = 0 #contador das rodadas
+        while len(self.__jogadores) > self.__qtdVencedores: #laço que controla as iterações da jogada de acordo com a regra estabelecida.
+            num_rodada += 1
+            self.rodada(num_rodada)#nossa função rodada é chamada aqui, de forma que nela, o jogo de fato começa a funcionar de acordo com as regras pré-estabelecidas.
+
+        print(f'Vencedores apos {num_rodada} rodadas: {self.__jogadores}')
+        print(f'Percurso para vitoria: {self.__removidos}')
+
+
+#
+#     def __str__(self)->str:
+#         s = 'Eliminados: '
+#         cursor = self.__topo
+#         while( cursor is not None ):
+#             s += f'{cursor.carga}, '
+#             cursor = cursor.prox
+#         s = s.rstrip('< ')
+#         s += ' '
+#         return s
+#
+#     def salvar_jogo(nome):
+#         """
+#         Método que salva a rodada do jogo atual.
+#         """
+#         jogo = open('jogo.txt','a')
+#         jogo.write (str) # o que é isso? esta certo mesmo?
--- main.py
+++ main.py
@@ -2,17 +2,25 @@ from jogo import *
 """
 Aqui é instanciado o objeto da classe Jogo.
 """
-rodada1 = Jogo()
+jogo = Jogo()
 
 
 try:
-    qtdJogadores = int(input('Diga a quantidade de jogadores: '))
-    qtdVencedores = int(input('Diga a quantidade de vencedores no jogo: '))
-    for i in range(1, qtdJogadores+1):
-        rodada.inserir(i, input('Jogador: '))
-    rodada1.__inicializador__(qtdVencedores)
-    print(rodada)
-    print(pilha)
+    jogar = True
+    while jogar:
 
-except ListaException as ae:
+        qtd_jogadores = int(input('Diga a quantidade de jogadores: '))
+        for i in range(1, qtd_jogadores+1):
+            jogo.addParticipantes(i, input('Jogador: '))#i é a posição e o input é o/a participante, ou seja, são os parametros que exigidos na função addParticipantes lá da classe jogo.
+
+        qtd_vencedores = int(input('Diga a quantidade de vencedores no jogo: '))
+        jogo.addQtdVencedores(qtd_vencedores)
+
+        jogo.executar()#aqui o jogo é executado
+
+        resposta = input('Deseja jogar novamente?: (S)im/(N)ão')#quando o jogo acaba, sai do laço e entra nesse input para saber se o/a jogador/a quer jogar novamente
+        if resposta == "N":
+            jogar = False
+
+except ListaException as ae: #criar uma classe de exceção pra nossa classe jogo
     print(ae)
